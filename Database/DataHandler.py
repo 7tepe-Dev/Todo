acca558ff4065ce036a23d993f3e8991bbdf2f2b
mertsancar <mertsancar12@gmail.com>
2021-10-22 18:07:29 +0300

    Database init user.txt
    Veritabanı (user.txt) eklendi. Database classına yeni bir fonksiyon eklendi (writeToDb())

--- Database/DataHandler.py
+++ Database/DataHandler.py
@@ -8,68 +8,68 @@ class DataHandler:
               data = json.load(f)         
          return data
 
-     def createNewUser(self,data):
-         newObject = DataHandler()
-         database = newObject.readfromDB()
-         database.append(data)
+     def writeToDB(self, database):
          with open('user.txt', 'w') as outfile:
             json.dump(database, outfile)
+
+     def createNewUser(self,data):
+         handlerObject = DataHandler()
+         database = handlerObject.readfromDB()
+         #print(len(database))
+         database.append(data)
+         handlerObject.writeToDB(database)
 
      def addNewTask(self ,username, newTask):
-         newObject = DataHandler()
-         database = newObject.readfromDB()
+         handlerObject = DataHandler()
+         database = handlerObject.readfromDB()
          for user in database :
              if user.get("username") == username :
-                user.get("task").append(newTask)
+                user.get("tasks").append(newTask)
                 break
-         with open('user.txt', 'w') as outfile:
-            json.dump(database, outfile)    
+         handlerObject.writeToDB(database)    
 
      def removeNewTask(self ,username, oldTask):
-         newObject = DataHandler()
-         database = newObject.readfromDB()
+         handlerObject = DataHandler()
+         database = handlerObject.readfromDB()
          for user in database :
              if user.get("username") == username :
-                user.get("task").remove(oldTask)
+                user.get("tasks").remove(oldTask)
                 break
-         with open('user.txt', 'w') as outfile:
-            json.dump(database, outfile)
+         handlerObject.writeToDB(database)
 
      def checkLogin(self , username , password):
          newObject = DataHandler()
          database = newObject.readfromDB()
          for user in database :
              if user.get("username") == username and user.get("password") == password :
-                print("Başarılı")  
+                print("Başarılı")
+                print(user)  
                 break
              else:
                 print("Başarısız Giriş") 
      
-                
-                   
-           
+                          
 # Tests for Datahandler class
-database = DataHandler()
 """
+### Add new user to DB
+database = DataHandler()
 data1 = {"username" : "Mert","password": "123" , "tasks":["reading book","go to the school"]}
 data2 = {"username" : "Kaan","password": "abc" , "tasks":["do homework","do some exercise"]}
 data3 = {"username" : "Yusuf","password": "456" , "tasks":["study English","prepare for Math exam"]}
-allData = []
-allData.append(data1)
-allData.append(data2)
-allData.append(data3)
-
-
-database.writetoDB(allData)
-
-
-database.checkLogin("Mert", "123")
-
-
 data4 = {"username" : "Mehmet","password": "dfg" , "tasks":["go to gym","prepare for English exam"]}
+for data in [data1, data2, data3, data4]:
+    database.createNewUser(data)
+"""
 
-database.createNewUser(data4)
+"""
+### Check User in DB
+database = DataHandler()
+database.checkLogin("Mert", "123")
+"""
 
+"""
+### Add/remove task to/from DB
+database = DataHandler() 
 database.addNewTask("Mert","go home")
 database.removeNewTask("Yusuf","study English")
 """
